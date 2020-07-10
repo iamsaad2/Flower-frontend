e10f3cc538a2ba91e805d2a6f600ac63c131a4ff
iamsaad2 <saadbadat.1@gmail.com>
2020-07-09 22:05:22 -0400

    ui fixes

--- src/components/flowers/Flower.js
+++ src/components/flowers/Flower.js
@@ -7,33 +7,9 @@ import DeleteFlower from './DeleteFlower';
 const Flower = () => {
   const flowerContext = useContext(FlowerContext);
 
-  const { flowers, getFlowers } = flowerContext;
+  const { flowers, getFlowers, editDistance } = flowerContext;
 
   const url = new URL(window.location);
-
-  function editDistance(s1, s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-
-    var costs = new Array();
-    for (var i = 0; i <= s1.length; i++) {
-      var lastValue = i;
-      for (var j = 0; j <= s2.length; j++) {
-        if (i == 0) costs[j] = j;
-        else {
-          if (j > 0) {
-            var newValue = costs[j - 1];
-            if (s1.charAt(i - 1) != s2.charAt(j - 1))
-              newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-            costs[j - 1] = lastValue;
-            lastValue = newValue;
-          }
-        }
-      }
-      if (i > 0) costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-  }
 
   let flowerDistance = [];
 
@@ -51,78 +27,83 @@ const Flower = () => {
         <div className='flower-container'>
           {flowerDistance.map((flower) => (
             <Fragment>
-              <Tree
-                data={{
-                  name: flower.water,
-                  children: [
-                    {
-                      name: flower.earth,
+              <div>
+                <div className='tree'>
+                  <Tree
+                    data={{
+                      name: flower.water,
                       children: [
                         {
-                          name: flower.air,
+                          name: flower.earth,
                           children: [
                             {
-                              name: flower.fire,
-                              children: [],
+                              name: flower.air,
+                              children: [
+                                {
+                                  name: flower.fire,
+                                  children: [],
+                                },
+                                {
+                                  name: flower.fire2,
+                                  children: [],
+                                },
+                              ],
                             },
                             {
-                              name: flower.fire2,
-                              children: [],
+                              name: flower.air2,
+                              children: [
+                                {
+                                  name: flower.fire3,
+                                  children: [],
+                                },
+                                {
+                                  name: flower.fire4,
+                                  children: [],
+                                },
+                              ],
                             },
                           ],
                         },
-                        {
-                          name: flower.air2,
-                          children: [
-                            {
-                              name: flower.fire3,
-                              children: [],
-                            },
-                            {
-                              name: flower.fire4,
-                              children: [],
-                            },
-                          ],
-                        },
-                      ],
-                    },
 
-                    {
-                      name: flower.earth2,
-                      children: [
                         {
-                          name: flower.air3,
+                          name: flower.earth2,
                           children: [
                             {
-                              name: flower.fire5,
-                              children: [],
+                              name: flower.air3,
+                              children: [
+                                {
+                                  name: flower.fire5,
+                                  children: [],
+                                },
+                                {
+                                  name: flower.fire6,
+                                  children: [],
+                                },
+                              ],
                             },
                             {
-                              name: flower.fire6,
-                              children: [],
-                            },
-                          ],
-                        },
-                        {
-                          name: flower.air4,
-                          children: [
-                            {
-                              name: flower.fire7,
-                              children: [],
-                            },
-                            {
-                              name: flower.fire8,
-                              children: [],
+                              name: flower.air4,
+                              children: [
+                                {
+                                  name: flower.fire7,
+                                  children: [],
+                                },
+                                {
+                                  name: flower.fire8,
+                                  children: [],
+                                },
+                              ],
                             },
                           ],
                         },
                       ],
-                    },
-                  ],
-                }}
-                height={200}
-                width={400}
-              />
+                    }}
+                    height={200}
+                    width={400}
+                  />
+                </div>
+                <p className='flower-name'>{flower.name}</p>
+              </div>
 
               <DeleteFlower flower={flower} />
             </Fragment>
